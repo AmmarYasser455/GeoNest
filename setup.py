a77d78787296d5c120e8e742107a02a5e9a0d1f2
Ammar Yasser Abdel-Azim Ahmed <xboxonegamer522@gmail.com>
2026-01-23 00:55:43 +0200

    Modify setup.py for version and long description
    Updated version number and added long description from README.

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="geonest",
-    version="0.1.1",
-    description="Interactive Python Map library for GIS using ipyleaflet",
+    version="0.1.0",
     author="Ammar Yasser",
-    author_email="ammaryasr522@gmail.com",
+    description="Interactive Python Map library for GIS using ipyleaflet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[
-        "ipyleaflet>=0.17.0",
-        "ipywidgets>=8.0.0",
-        "geopandas>=0.13.0",
-        "shapely>=2.1.0",
-        "localtileserver>=0.2.0"
-    ],
     python_requires='>=3.10',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        "ipyleaflet",
+        "ipywidgets",
+        "geopandas",
+        "localtileserver",
     ],
+    license="MIT",
 )
-
-license="MIT",
